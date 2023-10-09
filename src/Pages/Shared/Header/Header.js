0fed9bae0516fc1d58090732aab9bd43371c27f7
Sawom <asawom250@gmail.com>
2023-10-10 00:17:41 +0600

    navbar issue done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -14,7 +14,6 @@ const Header = () => {
                         <img style={{width: '60px'}} src={Logo} alt="" />
                         <p className="font-bold normal-case text-3xl">Laptop Land</p>
                     </div>
-                    
                 </div>
             </div>
             {/* 2nd part */}
@@ -24,10 +23,10 @@ const Header = () => {
                 {/* start */}
                 <div className="navbar-start">
                     <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost text-white lg:hidden">
+                    <label tabIndex={0} className="btn btn-ghost text-white  lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    {/* responsive */}
+                    {/* responsive menu part */}
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         <li className='font-bold' > <Link to='/home'>Home</Link> </li>
                         <li className='font-bold' > <Link to='/laptops'>Laptops</Link> </li>
@@ -42,35 +41,42 @@ const Header = () => {
                             </ul>
                             </details>
                         </li>
+                        {/* user wise will be changed */}
+                        <li className='font-bold'><Link  > Dashboard </Link></li>
+                        <li>  <button className='font-bold'>Logout</button>  </li>
+                        <li> <Link className='font-bold' > Login</Link> </li>
                     </ul>
                     </div>
                 </div>
-                {/* mid */}
+                {/* mid side */}
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                    <li className='font-bold'> <Link to='/home'> <span className='text-white' > Home </span> </Link> </li>
-                    <li className='font-bold'> <Link to='/laptops'> <span className='text-white'> Laptops </span> </Link> </li>
-                    {/* submenu */}
-                    <li tabIndex={0}>
-                            <details>
-                            <summary className='font-bold text-white active'> <span className='text-white'>Features</span> </summary>
-                            <ul className="p-2 font-bold">
-                                <li> <Link to='/about' > About </Link> </li>
-                                <li> <Link to='/faq' >FAQS</Link> </li>
-                                <li> <Link to='/terms' >Terms & Condition</Link> </li>
-                            </ul>
-                            </details>
-                    </li>
+                        <li className='font-bold'> <Link to='/home'> <span className='text-white' > Home </span> </Link> </li>
+                        <li className='font-bold'> <Link to='/laptops'> <span className='text-white'> Laptops </span> </Link> </li>
+                        {/* submenu */}
+                        <li tabIndex={0}>
+                                <details>
+                                <summary className='font-bold text-white active'> <span className='text-white'>Features</span> </summary>
+                                <ul className="p-2 font-bold">
+                                    <li> <Link to='/about' > About </Link> </li>
+                                    <li> <Link to='/faq' >FAQS</Link> </li>
+                                    <li> <Link to='/terms' >Terms & Condition</Link> </li>
+                                </ul>
+                                </details>
+                        </li>
                     </ul>
                 </div>
                 {/* nav end */}
-                <div className="navbar-end flex-none">
+                
+                <div className='lg:navbar-end hidden lg:flex' >
                         <ul className="menu menu-horizontal  px-1">
-                        <li className='mx-2'><Link className="btn btn-sm mx-2" >Dashboard</Link></li>
-                        <li> <button className="btn btn-sm mx-2">Logout</button> </li>
-                        <li> <Link className="btn btn-sm mx-2">Login</Link> </li>
+                            <li className='mx-2'><Link className="btn btn-sm btn-ghost mx-2" > <span className='text-white'>Dashboard</span> </Link></li>
+                            <li> <button className="btn btn-sm btn-ghost mx-2 "> <span className='text-white'>Logout</span> </button> </li>
+                            <li> <Link className="btn btn-sm btn-ghost mx-2"> <span className='text-white'>Login</span> </Link> </li>
                         </ul>
                 </div>
+                
+                
                 
                 </div>
             </div>
