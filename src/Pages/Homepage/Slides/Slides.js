1d64c62941840625d569524862eab2892660056d
Sawom <asawom250@gmail.com>
2023-10-10 21:27:24 +0600

    slides

--- src/Pages/Homepage/Slides/Slides.js
+++ src/Pages/Homepage/Slides/Slides.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React  from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import pic1 from '../../../images/slide/pic1.png';
@@ -9,14 +9,27 @@ import pic5 from '../../../images/slide/pic5.png';
 import pic6 from '../../../images/slide/pic6.png';
 import './Slides.css';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
 const Slides = () => {
+   
+
     return (
         <div className='container mx-auto'>
-            <Carousel showArrows={true} autoPlay={true} loop={true} >
+           <Carousel 
+           className='backColor mySwiper p-5'
+           pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+            infiniteLoop showArrows={true} autoPlay={true} loop={true} >
                 {/* 1 */}
                 <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1' >
                     <div >
-                       <p className='text-xl mt-10 text-left px-5'>
+                       <p className='text-2xl mt-10 text-left p-2 font-bold'>
                         Laptop Land: Your One-Stop Destination for Cutting-Edge Laptops!
                         </p> 
                     </div>
@@ -27,7 +40,9 @@ const Slides = () => {
                 {/* 2 */}
                 <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 ' >
                     <div >
-                        
+                        <p className='text-2xl mt-10 text-left px-5 font-bold'>
+                            Discover the Future of Computing at Laptop Land. Visit Us Today!
+                        </p>
                     </div>
                     <div >
                         <img alt='pic2' src={pic2} className='w-full' />
@@ -36,7 +51,9 @@ const Slides = () => {
                 {/* 3 */}
                 <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 ' >
                     <div >
-                        
+                        <p className='text-2xl mt-10 text-left px-5 font-bold'>
+                            Upgrade Your Tech Game with Laptop Land - Quality Laptops at Unbeatable Prices!
+                        </p>
                     </div>
                     <div >
                         <img alt='pic3' src={pic3} className='w-full' />
@@ -45,7 +62,9 @@ const Slides = () => {
                 {/* 4 */}
                 <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 ' >
                     <div >
-                        
+                        <p className='text-2xl mt-10 text-left px-5 font-bold'>
+                            Laptop Land - Your Trusted Source for Top Brands and Exceptional Service. Visit Us Today!
+                        </p>
                     </div>
                     <div >
                         <img alt='pic4' src={pic4} className='w-full' />
@@ -54,8 +73,8 @@ const Slides = () => {
                 {/* 5 */}
                 <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 ' >
                     <div  >
-                        <p>
-                            
+                        <p className='text-2xl mt-10 text-left px-5 font-bold'>
+                            Experience the Laptop Land Difference - Quality, Value, and Expertise Combined!
                         </p>
                     </div>
                     <div >
@@ -65,13 +84,16 @@ const Slides = () => {
                 {/* 6 */}
                 <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 ' >
                     <div >
-                        dsgsrurururururutrrurururrrrrrr
+                        <p className='text-2xl mt-10 text-left px-5 font-bold'>
+                            Your Laptop Dreams Come True at Laptop Land - Visit Us Today!
+                        </p>
                     </div>
                     <div >
                         <img alt='pic6' src={pic6} className='w-full' />
                     </div>
                 </div>
             </Carousel>
+            
         </div>
     );
 };
