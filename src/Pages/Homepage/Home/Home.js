6e0ff19c4d15633841926bdbca473a0e4643bd28
Sawom <asawom250@gmail.com>
2023-10-11 23:56:55 +0600

    new arrival

--- src/Pages/Homepage/Home/Home.js
+++ src/Pages/Homepage/Home/Home.js
@@ -3,12 +3,14 @@ import Slides from '../Slides/Slides';
 import Brands from '../Brands/Brands';
 import CallUs from '../CallUs/CallUs';
 import Banner from '../Banner/Banner';
+import Products from '../NewProducts/Products';
 
 const Home = () => {
     return (
         <div>
            <Slides></Slides>
            <Brands></Brands>
+           <Products></Products>
            <CallUs></CallUs>
            <Banner></Banner> 
         </div>
--- src/Pages/Homepage/NewProducts/Products.js
+++ src/Pages/Homepage/NewProducts/Products.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CardProducts from './CardProducts';
 
 const Products = () => {
+    // load home data
+    const [homedata, setHomedata] = useState([]);
+
+    useEffect( ()=>{
+        fetch('http://localhost:5000/homedata')
+            .then(res => res.json())
+            .then( data =>{
+                setHomedata(data);
+                // console.log(data);
+            } )
+    } , [])
+
     return (
-        <div>
+        <div className='container mx-auto my-10'>
+            <h1 className='text-center lg:text-4xl md:text-3xl text-xl my-10 font-bold' >
+                <span className=' py-4 border-y-4 uppercase' >new arrival </span> 
+            </h1>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-8 my-10'>
+                {
+                    homedata.map( (laptopInfo) => <CardProducts
+                         key={laptopInfo._id} laptopInfo={laptopInfo}
+                    ></CardProducts> )
+                }
+            </div>
+            {/* button responsive */}
+            <div className='text-center my-5'>
+                <button style={{backgroundColor: '#212E52'}} className="btn mt-5 text-white btn-outline btn-active btn-xs sm:btn-sm md:btn-md lg:btn-md ">View All Collections</button>
+            </div>
             
         </div>
     );
