471dfc20c48aa56516753bbca848a178fea8019e
Sawom <asawom250@gmail.com>
2023-10-11 20:28:11 +0600

    brands addeed

--- src/Pages/Homepage/Brands/Brands.js
+++ src/Pages/Homepage/Brands/Brands.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import asus from '../../../images/brands/Asus.png';
+import apple from '../../../images/brands/apple.png';
+import hp from '../../../images/brands/hps.png';
+import lenovo from '../../../images/brands/lenovo.png';
 
 const Brands = () => {
     return (
-        <div className='container mx-auto'>
-            
+        <div className='container mx-auto my-10'>
+            <h1 className='text-center lg:text-4xl md:text-3xl text-xl my-6 font-bold' >
+                <span className=' py-4 border-y-4 uppercase' >Our Brands </span> 
+            </h1>
+           <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper my-10"
+      >
+        <SwiperSlide> <img className='max-w-full h-auto' src={asus} alt="asus" /> </SwiperSlide>
+        <SwiperSlide> <img className='max-w-full h-auto' src={apple} alt="apple" /> </SwiperSlide>
+        <SwiperSlide> <img className='max-w-full h-auto' src={hp} alt="hp" /> </SwiperSlide>
+        <SwiperSlide> <img className='max-w-full h-auto' src={lenovo} alt="lenovo" /> </SwiperSlide>
+        
+      </Swiper> 
         </div>
     );
 };
