40251558dde9e52dee28a1826f41ae26ff791ce2
Sawom <asawom250@gmail.com>
2023-10-17 23:44:17 +0600

    updated

--- src/Pages/Authentication/useFirebase/useFirebase.js
+++ src/Pages/Authentication/useFirebase/useFirebase.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import {GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut, }  from "firebase/auth";
 import initializeFirebase from '../Firebase/firebase.init';
+import Swal from 'sweetalert2';
+import axios from 'axios';
 
 initializeFirebase();
 
 const useFirebase = () => {
     const [user,setUser] = useState({});
     const [ error, setError] = useState('');
+    const [loading, setLoading] = useState(true);
 
     const auth = getAuth();
 
@@ -18,7 +21,29 @@ const useFirebase = () => {
             .then((result) => {
                 const credential = GoogleAuthProvider.credentialFromResult(result);
                 const token = credential.accessToken;
-                const user = result.user;
+                const loggedInUser = result.user;
+                const saveUser = {name:loggedInUser.displayName, email: loggedInUser.email}
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers:{
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(saveUser)
+                })
+                .then(res => res.json())
+                .then( ()=>{
+                     Swal.fire({
+                            title: 'User Login Successful!',
+                            showClass: {
+                                popup: 'animate__animated animate__fadeInDown'
+                            },
+                            hideClass: {
+                                popup: 'animate__animated animate__fadeOutUp'
+                                }
+                        })
+
+                } )
+                setError('');
             }).catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
@@ -29,11 +54,19 @@ const useFirebase = () => {
 
      // observer if user signin or not
     useEffect(()=>{
-        const unsubscribed = onAuthStateChanged(auth, (user)=>{
-                if(user){
-                    setUser(user);
+        const unsubscribed = onAuthStateChanged(auth, (currentUser)=>{
+                if(currentUser){
+                    setUser(currentUser);
+                    axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+                    .then(data =>{
+                        // console.log(data.data.token);
+                        localStorage.setItem('access-token', data.data.token)
+                        setLoading(false);
+                    })
+
                 } else{
                     setUser({})
+                    localStorage.removeItem('access-token')
                 }
         });
         return () => unsubscribed;
@@ -44,7 +77,8 @@ const useFirebase = () => {
         signOut(auth).then(() => {
             // Sign-out successful.
         }).catch((error) => {
-            // An error happened.
+            const errorMessage = error.message;
+            setError(error.message);
         })
     }
 
