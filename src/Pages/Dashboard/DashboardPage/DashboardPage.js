b45cd62c16412a26d98e539ff33c93692907f90c
Sawom <asawom250@gmail.com>
2023-10-18 18:08:57 +0600

    updated

--- src/Pages/Dashboard/DashboardPage/DashboardPage.js
+++ src/Pages/Dashboard/DashboardPage/DashboardPage.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import useCart from '../../../Hooks/useCart';
+import useAdmin from '../../../Hooks/useAdmin';
+import { Link, Outlet } from 'react-router-dom';
 
 const DashboardPage = () => {
+    const [cart] = useCart();
+    const [isAdmin] = useAdmin();
+
     return (
-        <div>
-            dash page
+        <div className='overflow-x-auto h-full' >
+            <div className="drawer lg:drawer-open md:drawer-open drawer-open">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col items-center justify-center">
+                    {/* Page content here */}
+                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden  md:hidden hidden">Open drawer</label>
+                    <Outlet></Outlet>
+                </div> 
+                <div className="drawer-side bg-[#212E52]">
+                    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
+                    <ul className="menu p-2 w-auto min-h-full  text-base-content">
+                    {/* Sidebar content here */}
+                    <li className='font-bold'> <Link to='/home'> <span className='text-white' > Home </span> </Link> </li>
+                    <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                
+                </div>
+            </div>
         </div>
     );
 };
