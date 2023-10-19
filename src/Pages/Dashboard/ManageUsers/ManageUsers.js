8cf0f521774ebe8cd66e69612cc9bdbb6946f4ab
Sawom <asawom250@gmail.com>
2023-10-19 14:28:24 +0600

    added

--- src/Pages/Dashboard/ManageUsers/ManageUsers.js
+++ src/Pages/Dashboard/ManageUsers/ManageUsers.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const ManageUsers = () => {
+    const [axiosSecure] = useAxiosSecure();
+    const {data: users= [], refetch } = useQuery(['users'] , async () =>{
+        const res = await axiosSecure.get('/users')
+        return res.data ;
+    })
+
     return (
         <div>
-            manage all users
+            
         </div>
     );
 };
