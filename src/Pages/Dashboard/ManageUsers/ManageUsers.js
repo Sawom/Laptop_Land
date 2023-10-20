3ce66dd874ebeafbb21e3a55a80e2dfd3ba188e5
Sawom <asawom250@gmail.com>
2023-10-20 22:14:26 +0600

    updated

--- src/Pages/Dashboard/ManageUsers/ManageUsers.js
+++ src/Pages/Dashboard/ManageUsers/ManageUsers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaTrashAlt, FaUserShield } from 'react-icons/fa';
 import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import Swal from 'sweetalert2';
@@ -67,8 +68,45 @@ const ManageUsers = () => {
     } 
 
     return (
-        <div>
-            
+        <div className='container px-1'>
+            <h3 className='text-2xl font-semibold '> Total users: {users.length} </h3>
+             {/* table */}
+           <div className="overflow-x-auto">
+                <table className="table ">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {/* row 1 */}
+                    {
+                        users.map((user, index) => <tr key={user._id} >
+                            <th>{index + 1}</th>
+                            <td> {user.name} </td>
+                            <td> {user.email} </td>
+                            {/* check admin  */}
+                            <td>
+                                {/* user admin hole admin likha ta dekhabo ze beday admin. */}
+                                {
+                                    user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost bg-blue-700
+                                      text-white"> <FaUserShield></FaUserShield> </button>
+                                }
+                            </td>
+                            {/* delete button */}
+                            <td> 
+                                <button onClick={() => handleDeleteUser(user)} className="btn btn-ghost bg-red-600  text-white"><FaTrashAlt></FaTrashAlt></button>
+                            </td>
+                        </tr> )
+                    }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
